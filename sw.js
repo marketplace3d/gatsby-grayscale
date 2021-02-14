b646f5bbbd4939a9fd6091ee31d81c6c620ff398
marketplace3d <globe3d@hotmail.com>
2021-02-14 05:10:59 +0000

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-4648f04ff95d36edb77f.js"
+    "url": "webpack-runtime-c22cf61646c044e59f27.js"
   },
   {
     "url": "framework-dbb498007a7447f28d8e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "1825b65ee6c3263d705804208e3e34fd"
+    "revision": "c2d43062002c31e56a31f39816a5b59c"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "a3f5b321615d01c9af2207ea24799d89"
+    "revision": "4f9077e3bfa56fd082264fde20436f6f"
   },
   {
     "url": "polyfill-e4957c11759bdfc149d9.js"
